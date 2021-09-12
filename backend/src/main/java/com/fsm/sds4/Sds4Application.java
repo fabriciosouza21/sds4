25910b15c2c1f2904cf2457735c3686db47fcd89
fabricio21777 <fabriciosouza21777@gmail.com>
2021-09-12 16:41:23 -0300

    first commit

---
+++ backend/src/main/java/com/fsm/sds4/Sds4Application.java
@@ -0,0 +1,13 @@
+package com.fsm.sds4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sds4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sds4Application.class, args);
+	}
+
+}
