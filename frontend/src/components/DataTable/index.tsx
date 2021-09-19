c5ece1117c69c6a8b17ad6fe2bb7eda304ac1a74
fabricio21777 <fabriciosouza21777@gmail.com>
2021-09-19 14:52:04 -0300

    update path type/Sale

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import Pagination from "components/Pagination";
 import { useEffect, useState } from "react";
-import { SalePage } from "types/sale";
+import { SalePage } from "types/Sale";
 import { formatLocalDate } from "utils/Format";
 import { BASE_URL } from "utils/Request";
 
