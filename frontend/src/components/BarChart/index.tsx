ed79d7ecc94aeceeb88429bd6512285bbd53a686
fabricio21777 <fabriciosouza21777@gmail.com>
2021-09-19 13:09:43 -0300

    BarChart integration

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -1,6 +1,56 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 import Chat from 'react-apexcharts'
+import { SaleSucess } from 'types/Sale'
+import { round } from 'utils/Format'
+import { BASE_URL } from 'utils/Request'
 
+type SeriesData = {
+    name:string;
+    data:number[]
+}
+
+type ChartData = {
+    labels: {
+        categories: string[];
+    };
+    series: SeriesData[]
+}
 const Barchart = () => {
+    
+    const [chatrData, setChartData] = useState<ChartData>({
+        labels: {
+            categories: []
+        },
+        series: [
+            {
+                name: "",
+                data: []                   
+            }
+        ]
+    })
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/sucess-by-seller`)
+            .then(response => {
+
+                const data = response.data as SaleSucess[]
+                const myLabels = data.map((x) => x.sellerName)
+                const mySeries = data.map((x) => round(100 * x.deals/x.visited,1))
+                setChartData({
+                    labels: {
+                        categories: myLabels
+                    },
+                    series: [
+                        {
+                            name: "% Sucess",
+                            data: mySeries                   
+                        }
+                    ]
+                })
+            })
+    }, [])
+    
     const options = {
         plotOptions: {
             bar: {
@@ -8,24 +58,13 @@ const Barchart = () => {
             }
         },
     };
-    
-    const mockData = {
-        labels: {
-            categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-        },
-        series: [
-            {
-                name: "% Sucesso",
-                data: [43.6, 67.1, 67.7, 45.6, 71.1]                   
-            }
-        ]
-    };
-    
+
+   
     return (
 
         <Chat 
-        options ={{...options,xaxis: mockData.labels,}}
-        series = {mockData.series}
+        options ={{...options,xaxis: chatrData.labels,}}
+        series = {chatrData.series}
         type = 'bar'
         height = '240'
 
